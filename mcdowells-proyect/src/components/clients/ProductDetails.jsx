01d6b8b877feaed2fadbf86d80a6dfc3c8eed860
PauloBazan <paulobazan155@gmail.com>
2023-02-01 23:46:15 +0100

    otro cambio

--- mcdowells-proyect/src/components/clients/ProductDetails.jsx
+++ mcdowells-proyect/src/components/clients/ProductDetails.jsx
@@ -9,7 +9,7 @@ import { useCartContext } from '../../context/ShoppingCartContext';
 
 
 function ProductDetails() {
-    const context = useAppContext()
+    const context = useCartContext()
 
     const navigate = useNavigate();
     const [menu, setMenu] = useState([]);
@@ -26,16 +26,15 @@ function ProductDetails() {
         getProduct();
     }, [])
 
-    return (
-        <>
-            <div className="containerDetails">
-                <div className='leftContainerDetails'></div>
-                <div className="centerContainerDetails">
-                    <div className='topDetails'>
-                        <img className='detailsTitle' src={details} alt='NOT FOUND' />
-                    </div>
-                    {menu.map((one) =>
-                        <div className='detailsContainer' key={one.id_product}>
+    return (<>
+        <div className="containerDetails">
+            <div className='leftContainerDetails'></div>
+            <div className="centerContainerDetails">
+                <div className='topDetails'>
+                    <img className='detailsTitle' src={details} alt='NOT FOUND' />
+                </div>
+                {menu.map((one) =>
+                    <div className='detailsContainer' key={one.id_product}>
                         <p className='menu'>{one.name}</p>
 
                         <div className='centerDetails'>
@@ -44,34 +43,25 @@ function ProductDetails() {
                                 {product.description}
                             </p>
                         </div>
-                    )}
-                    <div className='bottomMenu'>
-                        <div className='bottomLeftDetails' >
-                            <button className='add'>AÑADIR AL PEDIDO</button>
-                        </div>
-                        <div className='bottomRightDetails'>
-                            <button className='select ' onClick={() => navigate(`/menus`)}>VOLVER A SELECCIÓN</button>
-
-                        </div>
+                    </div>)
+                }
+                <div className='bottomMenu'>
+                    <div className='bottomLeftDetails' >
+                        <button className='add'>AÑADIR AL PEDIDO</button>
+                    </div>
+                    <div className='bottomRightDetails'>
+                        <button className='select ' onClick={() => navigate(`/menus`)}>VOLVER A SELECCIÓN</button>
 
                     </div>
 
                 </div>
-                <div className='rigthContainerMenu'>
-                </div>
-            </div>
-        </>
-    )
-}
 
+            </div>
+            <div className='rigthContainerMenu'>
+            </div>
+        </div>
+    </>)
+
+}
 export default ProductDetails;
 
-/*
-                                Hamburguesa de tierna carne de vaca Wagyū, hecha en plancha Jasper a 180ºC para
-                                conseguir el punto exacto.
-                                <br />
-                                Con queso parmesano y queso americano ligeramente ahumado, bacon crispy, lechuga batavia, pomodoro y cebolla morada.
-                                <br />
-                                Todo ello acompañado de nuestras patatas bastón ecológicas cortadas en el día y cocinadas en aceite de oliva.
- 
-*/
